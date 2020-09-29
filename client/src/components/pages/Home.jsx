2d53c21d965fc5aac688db6ee71b17da213deba6
Anisio Mandlate <anisiomandlate@gmail.com>
2020-09-29 04:41:20 +0200

    feat: Add ContactForm component

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Contacts from "../Contacts/Contacts";
+import ContactForm from "../Contacts/ContactForm";
 
 const Home = () => {
   return (
     <div className="grid-2">
-      <div>{/** Contact Form */}</div>
+      <div>
+        <ContactForm />
+      </div>
       <div>
         <Contacts />
       </div>
