f03265a0f8f4bc0d11eff7f5f8cf142baa23e971
Anisio Mandlate <anisiomandlate@gmail.com>
2020-09-30 18:24:58 +0200

    feat: Add delete and edit function

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -37,6 +37,7 @@ const ContactState = (props) => {
         type: "Professional",
       },
     ],
+    current: null,
   };
   /** Pull out and dispatch the state from the reducer */
 
@@ -51,9 +52,21 @@ const ContactState = (props) => {
 
   /** Delete contact */
 
+  const deleteContact = (id) => {
+    dispatch({ type: DELETE_CONTACT, payload: id });
+  };
+
   /** Set current contact */
 
+  const setCurrent = (contact) => {
+    dispatch({ type: SET_CURRENT, payload: contact });
+  };
+
   /** Clear current contact */
+
+  const clearCurrent = () => {
+    dispatch({ type: CLEAR_CURRENT });
+  };
 
   /** Update contact */
 
@@ -65,7 +78,11 @@ const ContactState = (props) => {
     <ContactContex.Provider
       value={{
         contacts: state.contacts,
+        current: state.current,
         addContact,
+        deleteContact,
+        setCurrent,
+        clearCurrent,
       }}
     >
       {props.children}
