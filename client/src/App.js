90d7eb49727d6f138871f800c06316d5c52617da
Anisio Mandlate <anisiomandlate@gmail.com>
2020-10-02 17:04:07 +0200

    feat: Add PrivateRoute for home component

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import AuthState from "./context/auth/AuthState";
 import AlertState from "./context/alert/AlertState";
 import setAuthToken from "./utils/setAuthToken";
 import "./App.css";
+import PrivateRoute from "./components/Routing/PrivateRoute";
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -27,7 +28,7 @@ function App() {
               <div className="container">
                 <Alerts />
                 <Switch>
-                  <Route exact path="/" component={Home} />
+                  <PrivateRoute exact path="/" component={Home} />
                   <Route exact path="/about" component={About} />
                   <Route exact path="/register" component={Register} />
                   <Route exact path="/login" component={Login} />
