900e00ab5aff8e927912c94cb03b31abe23bc593
Anisio Mandlate <anisiomandlate@gmail.com>
2020-10-02 12:07:39 +0200

    style: Add border-radius on alerts

--- client/src/components/Layout/Alerts.jsx
+++ client/src/components/Layout/Alerts.jsx
@@ -1,16 +1,21 @@
 import React, { useContext } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import AlertContext from "../../context/alert/alertContext";
 
 const Alerts = () => {
   const alertContext = useContext(AlertContext);
 
   return (
-    alertContext.alerts.length !== 0 &&
-    alertContext.alerts.map((alert) => (
-      <div key={alert.id} className={`alert alert-${alert.type}`}>
-        <i className="fas fa-info-circle" /> {alert.msg}
-      </div>
-    ))
+    <TransitionGroup>
+      {alertContext.alerts.length !== 0 &&
+        alertContext.alerts.map((alert) => (
+          <CSSTransition key={alert.id} timeout={500} classNames="item">
+            <div className={`alert alert-${alert.type}`}>
+              <i className="fas fa-info-circle" /> {alert.msg}
+            </div>
+          </CSSTransition>
+        ))}
+    </TransitionGroup>
   );
 };
 
