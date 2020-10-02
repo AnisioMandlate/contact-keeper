cdf6287196e0af6527a3641ce3603abdf8f8bcf1
Anisio Mandlate <anisiomandlate@gmail.com>
2020-10-02 16:35:54 +0200

    feat: Add login context and login function

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -63,8 +63,20 @@ const AuthState = (props) => {
 
   /** Login User*/
 
-  const login = () => {
-    console.log("Loading User");
+  const login = async (formData) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    try {
+      const res = await axios.post("/api/auth", formData, config);
+      dispatch({ type: LOGIN_SUCCESS, payload: res.data });
+      loadUser();
+    } catch (error) {
+      dispatch({ type: LOGIN_FAIL, payload: error.response.data.msg });
+    }
   };
 
   /** Logout User*/
